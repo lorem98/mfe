b40ce4527778828da76320c629733889c6ba2f3e
lorem98 <lorenzo.medda24@gmail.com>
2026-02-13 12:13:33 +0100

    Temp commit

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,6 +4,8 @@ import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// Create a new class name generator to ensure the production build generates unique class names for the marketing app.
+// This is required to avoid class name collisions between the container and marketing apps in production.
 const generateClassName = createGenerateClassName({
   productionPrefix: "ma",
 });
