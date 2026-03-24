2be6f5463caa575b168db4f20996cdd05f7349bd
lorem98 <lorenzo.medda24@gmail.com>
2026-03-24 17:02:11 +0100

    Navigation between orchestrator and MF and viceversa

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,25 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        // Prevent unnecessary updates to the container's history if the marketing app's history has already been updated to match the container's history.
+        if (pathname !== nextPathname) {
+          // Update the container's history to match the marketing app's history
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // Create a memory history for the marketing app
+  /* This is required to allow the marketing app to use its own routing in isolation from the container app.
+  Avoid using BrowserHistory in the marketing app as it will cause issues with routing 
+  when the marketing app is used in isolation or when 
+  the container app is refreshed on a route other than the root route. 
+  If we are in development and in isolation, use BrowserHistory to enable navigation via the address bar.
+  */
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation
@@ -13,7 +37,7 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createMemoryHistory() });
   }
 }
 
